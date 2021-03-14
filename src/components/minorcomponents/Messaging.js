da97d96373d456960c1310d06fc45d31354abb48
Your Name <you@example.com>
2021-03-13 21:35:53 -0500

    made it possible to send email, should make an alert saying email has been sent or whatever

--- src/components/minorcomponents/Messaging.js
+++ src/components/minorcomponents/Messaging.js
@@ -1,51 +1,57 @@
-import React from "react";
-require("dotenv").config();
-const nodemailer = require("nodemailer");
+// import React from "react";
+// require("dotenv").config();
+// const nodemailer = require("nodemailer");
 
 function Messaging(props) {
 
-  const handleClick = async (e) => {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "RastaImagery@gmail.com",
-        pass: process.env.REACT_APP_PASSWORD,
-      },
-    });
+  // process.env.REACT_APP_YOUR_SERVICE_ID, process.env.REACT_APP_YOUR_TEMPLATE_ID, e.target, process.env.REACT_APP_YOUR_USER_ID
 
-    const info = {
-      from: props.emailInfo,
-      to: "rastaimagery@gmail.com",
-      subject: "Website Message",
-      text: props.messageInfo,
-    };
+  // const handleClick = async (e) => {
+  //   const transporter = nodemailer.createTransport({
+  //     service: "gmail",
+  //     auth: {
+  //       user: "RastaImagery@gmail.com",
+  //       pass: process.env.REACT_APP_PASSWORD,
+  //     },
+  //   });
 
-    transporter.sendMail(info, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
+  //   const info = {
+  //     from: props.emailInfo,
+  //     to: "rastaimagery@gmail.com",
+  //     subject: "Website Message",
+  //     text: props.messageInfo,
+  //   };
 
-    console.log("Message sent: %s", info.to);
-    console.log("from", info.from)
-    console.log("to", info.to)
-    console.log("subject", info.subject)
-    console.log("text", info.text)
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Comming back as undefined
-  };
+  //   transporter.sendMail(info, function (error, info) {
+  //     if (error) {
+  //       console.log(error);
+  //     } else {
+  //       console.log("Email sent: " + info.response);
+  //     }
+  //   });
+
+  //   console.log("Message sent: %s", info.to);
+  //   console.log("from", info.from)
+  //   console.log("to", info.to)
+  //   console.log("subject", info.subject)
+  //   console.log("text", info.text)
+  //   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+//   };
 
   return (
     <div>
-      <form>
-        <button type="button" id="submitBtn" onClick={handleClick}>
-          Submit
-        </button>
-      </form>
+
     </div>
-  );
+  )
 }
+//     <div>
+//       <form>
+//         <button type="button" id="submitBtn" onClick={handleClick}>
+//           Submit
+//         </button>
+//       </form>
+//     </div>
+//   );
+
 
 export default Messaging;
--- src/components/minorcomponents/SidebarRight.js
+++ src/components/minorcomponents/SidebarRight.js
@@ -1,24 +1,45 @@
 import Messaging from '../minorcomponents/Messaging'
+import React from "react";
+import emailjs from 'emailjs-com';
+require("dotenv").config();
+
 function SidebarRight(props) {
+    function sendEmail(e) {
+        console.log(props)
+        e.preventDefault();
+        emailjs.init(process.env.REACT_APP_USERID)
+        emailjs.send(process.env.REACT_APP_SERVICE_ID,process.env.REACT_APP_TEMPLATE_ID,{
+            nameInfo: props.nameInfo,
+            messageInfo: props.messageInfo,
+            phoneInfo: props.phoneInfo,
+            emailInfo: props.emailInfo,
+            })
+        .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      }
     return(
-        // use prevent default
+
+        
         <div id="rightSidebar">
             <p id="description">Enter the information below to send questions about bookings, pricing and questions</p>
-            <form id="inputLoc">
+            <form id="inputLoc" className="contact-form" onSubmit={(e) => sendEmail(e)}>
                 <label>Name:</label>
-                <input className="inputs" type="text" onChange={props.handleName} placeholder="John Doe"/>
+                <input className="inputs" type="text" onChange={props.handleName} placeholder="John Doe" name="user_email"/>
                 <label>Email:</label>
-                <input className="inputs" type="text" onChange={props.handleEmail} placeholder="JohnD@jd.com"/>
+                <input className="inputs" type="text" onChange={props.handleEmail} placeholder="JohnD@jd.com" name="user_name"/>
                 <label>Message:</label>
-                <input className="inputs" type="text" onChange={props.handleContent} placeholder="Enter Message"/>
+                <input className="inputs" type="text" onChange={props.handleContent} placeholder="Enter Message" name="message"/>
                 <h3>Optional</h3>
                 <label>Phone:</label>
                 <input className="inputs" type="text" onChange={props.handlePhone} placeholder="xxx-xxx-xxxx"/>
+                <input type="submit" id="submitBtn" />
                 <Messaging {...props}/>
-                {/* <Messaging messageInfo={props.messageInfo} phoneInfo={props.phoneInfo} nameInfo={props.nameInfo} emailInfo={props.emailInfo} asdf={props.asdf} /> */}
+
             </form>
 
-            {/* Suggestion was to clone props and pass them to messaging component */}
         </div>
     )
 }
